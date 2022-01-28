11a5b894859cd01194f886e1e9f874cf145a93e7
Gustavo Henrique <gustavohco34@gmail.com>
2022-01-28 01:13:27 -0300

    Aula 6

--- ContaBanco.java
+++ ContaBanco.java
@@ -1,71 +1,102 @@
 public class ContaBanco {
     //Atributos
+
     public int numConta;
     protected String tipo;
     private String dono;
     private float saldo;
     private boolean status = false;
 
+    // Construtor
+
     public ContaBanco() {
         this.saldo = 0;
         this.status = false;
     }
 
-    //Metodos
+    //Métodos
+
+    public void estadoAtual() {
+        System.out.println("-----------------------------");
+        System.out.println("Conta: "+this.getNumConta());
+        System.out.println("Tipo: "+this.getTipo());
+        System.out.println("Dono: "+this.getDono());
+        System.out.println("Saldo: "+this.getSaldo());
+        System.out.println("Status: "+this.getStatus());
+    }
 
     public void abrirConta(String t){
-        setTipo(t);
-        setStatus(true);
+        this.setTipo(t);
+        this.setStatus(true);
 
-        if(tipo == "CC"){ 
-            setSaldo(50.0f);;
-        } else if(tipo == "CP"){
-            setSaldo(150.0f);
+        if(t == "CC"){ 
+            this.setSaldo(50.0f);
+        } else if(t == "CP"){
+            this.setSaldo(150.0f);
         }
-
+        System.out.println("Conta aberta com sucesso! ");
     }
 
     public void fecharConta() {
-        if(this.saldo > 0 && this.status == true){
-            setStatus(false);
+        if(this.saldo > 0){
+            System.out.println("ERRO! a conta ainda possui saldo ");
         } else {
-            System.out.println("ERRO! conta em débito ou já está desativada. ");
+            this.setStatus(false);
+            System.out.println("Conta "+this.getNumConta()+" desativada com sucesso ");
+        } if (this.status == false) {
+            System.out.println("ERRO! conta desativada");
+        } else if(this.saldo > 0){
+            this.setStatus(false);
+            System.out.println("Conta "+this.getNumConta()+" desativada com sucesso ");
         }
     }
 
     public void depositar(float d) {
-        if(this.status == true) {
-            setSaldo(getSaldo()+d);;
+        if(this.getStatus()) {
+            this.setSaldo(this.getSaldo()+d);;
+            System.out.println("Deposito de "+d+"R$ realizado com sucesso na conta de "+this.getDono());
         } else {
             System.out.println("Impossível, Conta desativada ");
         }
     }
 
     public void sacar(float sq) {
-        if(getStatus() && getSaldo() > 0){
-            setSaldo(getSaldo()-sq);
+        if(this.getStatus()) {
+            if(sq > this.getSaldo()) {
+                System.out.println("Impossivel sacar, saldo insuficiente ");
+            } else {
+                this.setSaldo(this.getSaldo()-sq);
+                System.out.println("Saque de "+sq+" realizado com sucesso na conta de "+getDono());
+            }
         } else {
-            System.out.println("ERRO! Saldo insuficiente ou conta desativada");
+            System.out.println("Erro, conta desativada ");
+        } 
+        if (this.getSaldo() > 0) {
+            this.setSaldo(this.getSaldo()-sq);
+        } else {
+            System.out.println("Saldo insuficiente ");
         }
     }
 
     public void pagarMensal() {
-        float v;
-        if(getTipo() == "CC") {
+        float v = 0;
+
+        if(this.getTipo() == "CC") {
             v = 12;
-        } else if(getTipo() == "CP"){
+        } else if(this.getTipo() == "CP"){
             v = 20;
         }
         
-        if(getStatus()) {
+        if(this.getStatus()) {
             if(saldo > v){
-                setSaldo(getSaldo()-v);
+                this.setSaldo(this.getSaldo()-v);
             } else {
                 System.out.println("Saldo insuficiente ");
             }
         } else {
-            System.out.print("Impossível pagar");
+            System.out.print("Impossível pagar, conta desativada ");
         }
+        System.out.println("Mensalidade de "+this.getDono()+" paga com sucesso. ");
     }
 
     // Metodos especiais 
@@ -109,5 +140,4 @@ public class ContaBanco {
     public void setStatus(boolean tc){
         this.status = tc;
     }
-
 }
---
+++ aula06.java
@@ -0,0 +1,7 @@
+public class aula06 {
+    public static void main(String[] args) {
+        ControleRemoto c = new ControleRemoto();
+        c.ligar();
+        c.abrirMenu();
+    }
+}
