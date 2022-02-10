1598bf4802a3e09daa2e73932655ab210426076d
Gustavo Henrique <gustavohco34@gmail.com>
2022-02-10 01:15:20 -0300

    Aula 10

---
+++ ProjetoPessoas/src/Aluno.java
@@ -0,0 +1,24 @@
+public class Aluno extends Pessoa{
+    private int mat;
+    private String curso;
+
+    public void cancelarMatr() {
+        System.out.println("Matricula Cancelada");
+    }
+
+    public int getMat() {
+        return mat;
+    }
+
+    public void setMat(int mat) {
+        this.mat = mat;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+}
---
+++ ProjetoPessoas/src/principal.java
@@ -0,0 +1,18 @@
+public class principal {
+    public static void main(String[] args){
+        Pessoa p1 = new Pessoa();
+        Aluno p2 = new Aluno();
+        Professor p3 = new Professor();
+        Funcionario p4 = new Funcionario();
+
+        p1.setNome("Pedro");
+        p2.setNome("Maria");
+        p3.setNome("Cláudio");
+        p4.setNome("Fabiana");
+        
+        System.out.println(p1.toString());
+        System.out.println(p2.toString());
+        System.out.println(p3.toString());
+        System.out.println(p4.toString());
+    }
+}
